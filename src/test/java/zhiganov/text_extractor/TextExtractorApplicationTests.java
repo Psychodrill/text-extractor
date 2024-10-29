96f376fa7c44a0f61a9391ff20601bc4836c0ec1
Taras <zhiganovtaras@gmail.com>
2024-10-29 12:19:14 +0300

    Initial commit

---
+++ src/test/java/zhiganov/text_extractor/TextExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package zhiganov.text_extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
